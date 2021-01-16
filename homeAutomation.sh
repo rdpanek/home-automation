968ee1fb3c3091ccae9a179d7a777e6c99e23433
Radim Daniel Panek <rdpanek@gmail.cz>
2021-01-16 21:06:14 +0100

    homeAutomation.sh: refactoring

--- homeAutomation.sh
+++ homeAutomation.sh
@@ -4,19 +4,24 @@ ELASTICCLUSTER_URI=
 ELASTIC_AUTH=
 
 HUE_URI=
+HUE_USER=
 
 NETATMO_USER=
 NETATMO_PASS=
 NETATMO_CLIENT_ID=
 NETATMO_SECRET=
+# device_id is mac address of station
 NETATMO_DEVICE_ID=
+
+#{"place":"Pracovna","ip":"192.168.1.203"},
+#{"place":"Kuchyn","ip":"192.168.1.106"},
+#{"place":"Obyvak","ip":"192.168.1.181"},
+#{"place":"Loznice","ip":"192.168.1.166"}
 
 SMARWIS=$(cat <<EOF
 [
-  {"place":"Pracovna","ip":"192.168.1.203"},
-  {"place":"Kuchyn","ip":"192.168.1.106"},
   {"place":"Obyvak","ip":"192.168.1.181"},
-  {"place":"Loznice","ip":"192.168.1.167"}
+  {"place":"Loznice","ip":"192.168.1.166"}
   ]
 EOF
 )
@@ -31,8 +36,8 @@ echo $dt
 # +-------------------------------+
 
 # get list of lights
-list_of_lights=$(curl -X GET ${HUE_URI}/lights)
-
+list_of_lights=$(curl -X GET ${HUE_URI}/api/${HUE_USER}/lights)
+echo $list_of_lights
 # parse json and send to elasitc
 for row in $(echo "${list_of_lights}" | jq -r '.[] | @base64'); do
     _jq() {
@@ -41,7 +46,7 @@ for row in $(echo "${list_of_lights}" | jq -r '.[] | @base64'); do
 
    echo $(_jq '.name') $(_jq '.state.on') ${lightState}
    curl -X POST \
-    ${ELASTICCLUSTER_URI}/homeautomation-${elastic_index_date}/lights \
+    ${ELASTICCLUSTER_URI}/h.hue-${elastic_index_date}/lights \
     -H 'Authorization: Basic '${ELASTIC_AUTH} \
     -H 'Content-Type: application/json' \
     -H 'cache-control: no-cache' \
@@ -84,96 +89,55 @@ netatmo_senzors=$(curl -X POST \
   -H 'cache-control: no-cache' \
   -d 'access_token='${access_token}'&device_id='$NETATMO_DEVICE_ID'&undefined=')
 
-for senzor in $(echo "${netatmo_senzors}"); do
-    _jq() {
-     echo ${senzor} | jq -r ${1}
-    }
-    curl -X POST \
-     ${ELASTICCLUSTER_URI}/netatmo-${elastic_index_date}/senzors \
-     -H 'Authorization: Basic '${ELASTIC_AUTH} \
-     -H 'Content-Type: application/json' \
-     -H 'cache-control: no-cache' \
-     -d '{
-     "moduleType": "base",
-     "moduleName": "'$(_jq '.body.devices[0].module_name' | tr -d '[:space:]')'",
-     "temperature": '$(_jq '.body.devices[0].dashboard_data.Temperature')',
-     "co2": '$(_jq '.body.devices[0].dashboard_data.CO2')',
-     "humidity": '$(_jq '.body.devices[0].dashboard_data.Humidity')',
-     "noise": '$(_jq '.body.devices[0].dashboard_data.Noise')',
-     "pressure": '$(_jq '.body.devices[0].dashboard_data.Pressure')',
-     "batteryPercent": 0,
-     "place": "tovarni",
-     "timestamp": "'${dt}'"
-    }'
+curl -X POST \
+  ${ELASTICCLUSTER_URI}/h.netatmo-${elastic_index_date}/senzors \
+  -H 'Authorization: Basic '${ELASTIC_AUTH} \
+  -H 'Content-Type: application/json' \
+  -H 'cache-control: no-cache' \
+  -d '{
+  "moduleType": "base",
+  "moduleName": '$(echo $netatmo_senzors | jq '.body.devices[0].module_name')',
+  "temperature": '$(echo $netatmo_senzors | jq '.body.devices[0].dashboard_data.Temperature')',
+  "co2": '$(echo $netatmo_senzors | jq '.body.devices[0].dashboard_data.CO2')',
+  "humidity": '$(echo $netatmo_senzors | jq '.body.devices[0].dashboard_data.Humidity')',
+  "noise": '$(echo $netatmo_senzors | jq '.body.devices[0].dashboard_data.Noise')',
+  "pressure": '$(echo $netatmo_senzors | jq '.body.devices[0].dashboard_data.Pressure')',
+  "batteryPercent": 0,
+  "timestamp": "'${dt}'"
+}'
 
-    additional_modules=$(_jq '.body.devices[0].modules')
-
-done
+additional_modules=$(echo $netatmo_senzors | jq '.body.devices[0].modules')
 
 
 # +-------------------------------+
 # |  NETATMO ADDITIONAL MODUL     |
 # +-------------------------------+
-
 echo $additional_modules
 for module in $(echo "${additional_modules}" | jq -r '.[] | @base64'); do
-    _jq() {
-     echo ${module} | base64 --decode | jq -r ${1}
-    }
-    moduleName=$(_jq '.module_name' | tr -d '[:space:]')
-    temperature=$(_jq '.dashboard_data.Temperature')
-    humidity=$(_jq '.dashboard_data.Humidity')
-    co2=$(_jq '.dashboard_data.CO2')
-    # CO2 se u venkovniho modulu nemeri
-    if [ "$co2" != 'null' ]; then
-       co2=$co2
-    fi
-    batteryPercent=$(_jq '.battery_percent')
-   curl -X POST \
-    ${ELASTICCLUSTER_URI}/netatmo-${elastic_index_date}/senzors \
-    -H 'Authorization: Basic '${ELASTIC_AUTH} \
-    -H 'Content-Type: application/json' \
-    -H 'cache-control: no-cache' \
-    -d '{
-    "moduleType": "additional",
-    "moduleName": "'$moduleName'",
-    "temperature": '$temperature',
-    "co2": '$co2',
-    "humidity": '$humidity',
-    "noise": 0,
-    "pressure": 0,
-    "batteryPercent": '$batteryPercent',
-    "place": "tovarni",
-    "timestamp": "'${dt}'"
-   }'
-
-done
-
-# +-------------------------------+
-# |       SMARWI DEVICES          |
-# +-------------------------------+
-
-for smarwi in $(echo "${SMARWIS}" | jq -r '.[] | @base64'); do
-  _jq() {
-    echo ${smarwi} | base64 --decode | jq -r ${1}
-  }
-
-  echo $(_jq '.place') $(_jq '.ip')
-  device_result_string=$(curl -X GET $(_jq '.ip')/statusn)
-  if [[ "$device_result_string" == *"pos:o"* ]]; then
-    is_device_opened=true
-  else
-    is_device_opened=false
+  
+  moduleName=$(echo $module | base64 --decode | jq '.module_name')
+  temperature=$(echo $module | base64 --decode | jq '.dashboard_data.Temperature')
+  humidity=$(echo $module | base64 --decode | jq '.dashboard_data.Humidity')
+  co2=$(echo $module | base64 --decode | jq '.dashboard_data.CO2')
+  # CO2 se u venkovniho modulu nemeri
+  if [ "$co2" != 'null' ]; then
+      co2=$co2
   fi
-
+  batteryPercent=$(echo $module | base64 --decode | jq '.battery_percent')
   curl -X POST \
-  ${ELASTICCLUSTER_URI}/smarwi-${elastic_index_date}/windows \
+  ${ELASTICCLUSTER_URI}/h.netatmo-${elastic_index_date}/senzors \
   -H 'Authorization: Basic '${ELASTIC_AUTH} \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
   -d '{
-  "place": "'$(_jq '.place')'",
-  "is_open": '$is_device_opened',
-  "timestamp": "'${dt}'"
+    "moduleType": "additional",
+    "moduleName": '$(echo $moduleName)',
+    "temperature": '$(echo $temperature)',
+    "co2": '$(echo $co2)',
+    "humidity": '$(echo $humidity)',
+    "noise": 0,
+    "pressure": 0,
+    "batteryPercent": '$(echo $batteryPercent)',
+    "timestamp": "'${dt}'"
   }'
 done
